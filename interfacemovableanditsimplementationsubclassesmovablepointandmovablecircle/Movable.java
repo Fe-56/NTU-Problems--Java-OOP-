0a0e4ea5a4d51b7474bdd252d01f575a5c8075df
Fe-56 <limfuoen1999@gmail.com>
2022-04-28 21:10:34 +0800

    Movable Interface and its Implementation Classes MovablePoint and MovableCircle

---
+++ interfacemovableanditsimplementationsubclassesmovablepointandmovablecircle/Movable.java
@@ -0,0 +1,8 @@
+package interfacemovableanditsimplementationsubclassesmovablepointandmovablecircle;
+
+public interface Movable {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+}
---
+++ interfacemovableanditsimplementationsubclassesmovablepointandmovablecircle/MovableCircle.java
@@ -0,0 +1,36 @@
+package interfacemovableanditsimplementationsubclassesmovablepointandmovablecircle;
+
+public class MovableCircle implements Movable{
+    private int radius;
+    private MovablePoint center;
+
+    MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius){
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.center.x + "," + this.center.y + "),speed=(" + this.center.xSpeed + "," + this.center.ySpeed + "),radius=" + this.radius;
+    }
+
+    @Override
+    public void moveUp(){
+        this.center.y -= this.center.ySpeed;
+    }
+
+    @Override
+    public void moveDown(){
+        this.center.y += this.center.ySpeed;
+    }
+
+    @Override
+    public void moveLeft(){
+        this.center.x -= this.center.xSpeed;
+    }
+
+    @Override
+    public void moveRight(){
+        this.center.x += this.center.xSpeed;
+    }
+}
